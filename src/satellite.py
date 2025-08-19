02e7f00e24970ffb7b784f7701ba391455c32219
Maurice Meus <jmm974@student.bham.ac.uk>
2025-08-19 15:48:52 +0100

    Combine sattellite.py files

--- src/satellite.py
+++ src/satellite.py
@@ -2,16 +2,26 @@
 # MEW 12th Aug 2025
 
 from dataclasses import dataclass
-from .body import Body
-
-@dataclass
-class SurfaceProp:
-    pass
+import param
+import numpy as np
+from body import Body
 
 @dataclass
 class OrbitalProp:
-    pass
+    "A class for the orbital elements, using the Keplerian Parameterisation"
+    eccentricity:   float                                                           # eccentricity takes values in (0,1) for closed elliptical trajectories, 1 for parabolic trajectories and (1, inf) for hyperbolic orbits
+    semiMajorAxis:  float                                                           # semi-major axis takes values >0
+    periapsis:      float                                                           # periapsis takes values >0
+    inclination:    float = param.Number(default=0, bounds=(0, np.pi / 2))          # inclination angle in radians, takes values in [0, pi/2]
+    rightAscension: float = param.Number(default=0, bounds=(0, 2 * np.pi))          # right ascension, angle in radians, in (0, 2 pi)
+    true_anomaly:   float = param.Number(default=0, bounds=(0, 2 * np.pi))          # True anomaly, in (0, 2 * pi)
 
+@dataclass
+class SurfaceProp:
+    "A class storing properties of the surface of satellites."
+    albedo:     float = param.Number(default=0.5, bounds=(0,1))                  # Albedo value should be between 0 (perfect absorber) and 1 (perfect reflector)
+    emisivity:  float = param.Number(default=0.5, bounds=(0,1))                  # Emisivity value also between 0 (perfect reflector) and 1 (perfect emitter)
+    
 
 class Satellite(Body):
     
