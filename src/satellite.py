797f5ab2823fd6fc1f296bae338ede6b99f3b19b
Maurice Meus <jmm974@student.bham.ac.uk>
2025-08-20 10:57:36 +0100

    Close #17

--- src/satellite.py
+++ src/satellite.py
@@ -35,7 +35,7 @@ class Satellite(Body):
         # Placeholder for temperature calculation logic
         return 0.0
 
-    def get_coords(self):
+    def get_coords(self, time: float) -> tuple[float, float, float]:
 
         # Placeholder for coordinates calculation logic
         return (0.0, 0.0, 0.0)
